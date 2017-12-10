ac9cff1da7ec42fc6a9ecaa21f42ce03ad58d9ec
Joey Eremondi <joey.eremondi@gmail.com>
2017-12-09 21:58:01 -0800

    Attempt at expr-based SAT

--- src/SolveSetConstraints.hs
+++ src/SolveSetConstraints.hs
@@ -12,10 +12,10 @@ import SMTHelpers
 import qualified SimpleSMT as SMT
 import Syntax
 
-formulaForCExpr :: (Literal -> SMT.SExpr) -> CExpr -> SMT.SExpr
-formulaForCExpr litNum cexp =
+formulaForCExpr :: (Expr -> BitVector) -> CExpr -> SMT.SExpr
+formulaForCExpr exprNum cexp =
   case cexp of
-    (s1 `CSubset` s2) -> litNum $ Literal (s1, s2)
+    (s1 `CSubset` s2) -> (Fun "subsetof") $$$ [exprNum s1, exprNum s2]
     (CAnd cexprs) -> andAll $ map self cexprs
     (COr cexprs) -> orAll $ map self cexprs
     (c1 `CImplies` c2) -> self c1 ==> self c2
@@ -23,17 +23,18 @@ formulaForCExpr litNum cexp =
     (CXor cexprs) -> error "TODO XOR" --"xor" $$ [self c1, self c2]
     (CNot c1) -> SMT.not $ self c1
   where
-    self = formulaForCExpr litNum
+    self = formulaForCExpr exprNum
 
-makeLemma :: (Literal -> SMT.SExpr) -> [Constr] -> SMT.SExpr
-makeLemma litNum clist = SMT.not $ andAll $ map helper clist
+makeLemma :: (Expr -> BitVector) -> [Constr] -> SMT.SExpr
+makeLemma exprNum clist = SMT.not $ andAll $ map helper clist
   where
     helper c =
       case c of
-        e1 `Sub` e2 -> litNum $ Literal (e1, e2)
-        e1 `NotSub` e2 -> SMT.not $ litNum $ Literal (e1, e2)
+        e1 `Sub` e2 -> (Fun "subsetof") $$$ [exprNum e1, exprNum e2]
+        e1 `NotSub` e2 ->
+          SMT.not $ (Fun "subsetof") $$$ [(exprNum e1), (exprNum e2)]
+  --intToBits :: Integral i => i -> String
 
---intToBits :: Integral i => i -> String
 intToBits n i = BitVector $ map bitToSexp paddedBinaryString
   where
     asBinaryString = showIntAtBase 2 intToDigit i ""
@@ -44,50 +45,47 @@ intToBits n i = BitVector $ map bitToSexp paddedBinaryString
 
 solveSetConstraints :: SMT.Solver -> Options -> (Expr, CExpr) -> IO ()
 solveSetConstraints s options (nonEmptyExpr, cInitial)
-  --Declare our inclusion function
-  --
+    --Declare our inclusion function
+    --
  = do
   SMT.simpleCommand s ["set-logic", "UF"]
   SMT.simpleCommand s ["push"]
-  -- SMT.declareFun s "literalValue" litType SMT.tBool
-  forM literalNames $ \(SMT.Atom ln) -> SMT.declare s ln SMT.tBool
-  --Assert the SMT version of our expression
-  SMT.assert s $ formulaForCExpr litFun c
-  putStrLn "Starting to assert subset properties"
-  -- forM [(e1, e2) | e1 <- exprList, e2 <- exprList] $
-  --   uncurry assertSubsetProperty
-  putStrLn "Done asserting subset properties"
-  -- assertTransitive
-  putStrLn "Done asserting transitivity"
+  SMT.declareFun s "subsetof" (litType ++ litType) SMT.tBool
+    --Assert the SMT version of our expression
+  SMT.assert s $ formulaForCExpr exprFun c
+  putStrLn "Starting subset asserts"
+  forM [(e1, e2) | e1 <- exprList, e2 <- exprList] $
+    uncurry assertSubsetProperty
+  putStrLn "Starting transitive assert"
+  assertTransitive
+  putStrLn "Ending transitive assert"
   solverLoop 0
-    -- exprSubset lhs rhs = (Fun "literalValue") $$$ [exprFun lhs, exprFun rhs]
   where
+    exprSubset lhs rhs = (Fun "subsetof") $$$ [exprFun lhs, exprFun rhs]
     nonEmptyConstr = CNot (CSubset nonEmptyExpr Bottom)
     c = (CAnd [cInitial, nonEmptyConstr])
-    literalNames =
-      map (\i -> SMT.Atom $ "literal_" ++ show i) [0 .. length lits - 1]
     lits = literalsInCExpr c
-    litMap = Map.fromList $ flip zip literalNames $ Set.toList lits
-    litFun = (litMap Map.!)
     toFloat = (fromIntegral :: Int -> Float)
-    numBits = ceiling $ logBase 2 (toFloat $ Set.size lits)
+    numBits = ceiling $ logBase 2 (toFloat $ Set.size exprs)
     litType = replicate numBits SMT.tBool
     exprs = exprsInCExpr c
-    -- exprList = Set.toList exprs
-    -- exprMap = Map.fromList $ zip (Set.toList exprs) [0 ..]
-    -- exprFun = (intToBits numBits) . (exprMap Map.!)
+    exprList = Set.toList exprs
+    exprMap = Map.fromList $ zip (Set.toList exprs) [0 ..]
+    exprFun = (intToBits numBits) . (exprMap Map.!)
     solverLoop i = do
-      putStrLn $ "SolverLoop" ++ show i
+      putStrLn $ "Solver loop" ++ show i
       result <- SMT.check s
+      putStrLn "Check complete"
       case result of
         SMT.Unsat -> putStrLn $ "UNSAT in " ++ show i ++ " theory iterations"
         SMT.Unknown -> error "Shouldn't have quantifiers in solver loop"
         SMT.Sat -> do
-          putStrLn "Solver loop SAT, trying theory solver"
+          putStrLn "SAT, trying theory solver "
           model <- SMT.command s $ SMT.List [SMT.Atom "get-model"]
           litAssigns <-
-            forM (Set.toList lits) $ \lit@(Literal (lhs, rhs)) -> do
-              result <- SMT.getExpr s $ litFun lit
+            forM (Set.toList lits) $ \(lhs, rhs) -> do
+              result <-
+                SMT.getExpr s $ (Fun "subsetof") $$$ [exprFun lhs, exprFun rhs]
               let resultBool =
                     case result of
                       SMT.Bool b -> b
@@ -98,12 +96,12 @@ solveSetConstraints s options (nonEmptyExpr, cInitial)
                 True -> return $ lhs `Sub` rhs
                 False -> return $ lhs `NotSub` rhs
           SMT.simpleCommand s ["push"]
-          --putStrLn $ "Outer loop trying: " ++ show litAssigns
+            --putStrLn $ "Outer loop trying: " ++ show litAssigns
           result <- Solver.makePred s options (nonEmptyExpr, litAssigns) --TODO make better name
           SMT.simpleCommand s ["pop"]
           case result of
             Left lemma -> do
-              SMT.assert s $ makeLemma litFun lemma
+              SMT.assert s $ makeLemma exprFun lemma
               solverLoop (i + 1)
             Right _ ->
               putStrLn $ "SAT in " ++ show (i + 1) ++ " theory iterations"
@@ -119,41 +117,31 @@ solveSetConstraints s options (nonEmptyExpr, cInitial)
           [arg1, arg2, arg3] =
             map (map SMT.Atom) [arg1names, arg2names, arg3names]
           bodyCond =
-            (((Fun "literalValue") $$$ [BitVector arg1, BitVector arg2]) /\
-             ((Fun "literalValue") $$$ [BitVector arg2, BitVector arg3])) ==>
-            ((Fun "literalValue") $$$ [BitVector arg1, BitVector arg3])
+            (((Fun "subsetof") $$$ [BitVector arg1, BitVector arg2]) /\
+             ((Fun "subsetof") $$$ [BitVector arg2, BitVector arg3])) ==>
+            ((Fun "subsetof") $$$ [BitVector arg1, BitVector arg3])
       in SMT.assert s $ forAll typePairs bodyCond
-    -- assertTransitive =
-    --   forM
-    --     [ (e1, e2, e3)
-    --     | e1 <- exprList
-    --     , e2 <- exprList
-    --     , e3 <- exprList
-    --     , length (List.nub [e1, e2, e3]) == 3
-    --     ] $ \(e1, e2, e3) -> do
-    --     SMT.assert s $
-    --       ((exprSubset e1 e2) /\ (exprSubset e2 e3)) ==> (exprSubset e1 e3)
-    -- assertSubsetProperty :: Expr -> Expr -> IO ()
-    -- --Bottom subset of everything
-    -- assertSubsetProperty Bottom rhs = SMT.assert s $ exprSubset Bottom rhs
-    -- --Top superset of everything
-    -- assertSubsetProperty lhs Top = SMT.assert s $ exprSubset lhs Top
-    -- --Assume our lattice is not degenerate
-    -- assertSubsetProperty Top Bottom =
-    --   SMT.assert s $ SMT.not $ exprSubset Top Bottom
-    -- --Everything a subset of itself
-    -- assertSubsetProperty lhs rhs
-    --   | lhs == rhs = SMT.assert s $ exprSubset lhs rhs
-    -- -- A subset of A U B
-    -- assertSubsetProperty lhs rhs@(Union e1 e2)
-    --   | lhs `List.elem` [e1, e2] = SMT.assert s $ exprSubset lhs rhs
-    --   -- A /\ B subset of A
-    -- assertSubsetProperty lhs@(Intersect e1 e2) rhs
-    --   | lhs `List.elem` [e1, e2] = SMT.assert s $ exprSubset lhs rhs
-    -- --Constant symbols are never in the empty set
-    -- assertSubsetProperty lhs@(FunApp f []) Bottom =
-    --   SMT.assert s $ SMT.not $ exprSubset lhs Bottom
-    -- --Disjoint symbols are never subsets of each otheppr
-    -- assertSubsetProperty lhs@(FunApp f _) rhs@(FunApp g _)
-    --   | f /= g = SMT.assert s $ SMT.not $ exprSubset lhs rhs
-    -- assertSubsetProperty _ _ = return ()
+    assertSubsetProperty :: Expr -> Expr -> IO ()
+      --Bottom subset of everything
+    assertSubsetProperty Bottom rhs = SMT.assert s $ exprSubset Bottom rhs
+      --Top superset of everything
+    assertSubsetProperty lhs Top = SMT.assert s $ exprSubset lhs Top
+      --Assume our lattice is not degenerate
+    assertSubsetProperty Top Bottom =
+      SMT.assert s $ SMT.not $ exprSubset Top Bottom
+      --Everything a subset of itself
+    assertSubsetProperty lhs rhs
+      | lhs == rhs = SMT.assert s $ exprSubset lhs rhs
+      -- A subset of A U B
+    assertSubsetProperty lhs rhs@(Union e1 e2)
+      | lhs `List.elem` [e1, e2] = SMT.assert s $ exprSubset lhs rhs
+        -- A /\ B subset of A
+    assertSubsetProperty lhs@(Intersect e1 e2) rhs
+      | lhs `List.elem` [e1, e2] = SMT.assert s $ exprSubset lhs rhs
+      --Constant symbols are never in the empty set
+    assertSubsetProperty lhs@(FunApp f []) Bottom =
+      SMT.assert s $ SMT.not $ exprSubset lhs Bottom
+      --Disjoint symbols are never subsets of each otheppr
+    assertSubsetProperty lhs@(FunApp f _) rhs@(FunApp g _)
+      | f /= g = SMT.assert s $ SMT.not $ exprSubset lhs rhs
+    assertSubsetProperty _ _ = return ()
